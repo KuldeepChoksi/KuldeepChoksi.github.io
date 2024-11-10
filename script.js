0ee575165066d7912c82a6ed1c41eae02283b6b3
Kuldeep Choksi <KuldeepChoksi@Dragon-Mac.local>
2024-11-10 12:56:10 -0500

    new changes

--- script.js
+++ script.js
@@ -9,20 +9,6 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-// Skill Bars
-const skills = [
-    { name: 'C/C++', level: 90 },
-    { name: 'Python', level: 95 },
-    { name: 'JavaScript', level: 85 }
-];
-
-const skillsContainer = document.querySelector('.skills ul');
-skills.forEach(skill => {
-    const skillItem = document.createElement('li');
-    skillItem.innerHTML = `<strong>${skill.name}</strong>: ${skill.level}%`;
-    skillsContainer.appendChild(skillItem);
-});
-
 // Form submission
 const contactForm = document.getElementById('contact-form');
 contactForm.addEventListener('submit', (e) => {
